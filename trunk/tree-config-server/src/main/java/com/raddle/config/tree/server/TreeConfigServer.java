7ec0f5f8a9c15241d98afc41917b66fe195545bd
raddle60 <raddle60@b04a5dc0-1bbb-ae7b-cb6e-5c70aac61664>
2010-11-25 09:10:12 +0000

    server

---
+++ trunk/tree-config-server/src/main/java/com/raddle/config/tree/server/TreeConfigServer.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.raddle.config.tree.server;
+
+/**
+ * @author xurong
+ * 
+ */
+public class TreeConfigServer {
+	public void start() {
+
+	}
+
+	public void shutdown() {
+
+	}
+}
