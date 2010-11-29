38ac90d06ab3f98bc6995160013a9222d3422cb4
raddle60 <raddle60@b04a5dc0-1bbb-ae7b-cb6e-5c70aac61664>
2010-11-29 03:53:00 +0000

    基本的方法实现

--- trunk/tree-config-client/src/main/java/com/raddle/config/tree/client/TreeConfigClient.java
+++ trunk/tree-config-client/src/main/java/com/raddle/config/tree/client/TreeConfigClient.java
@@ -1,7 +1,10 @@
 package com.raddle.config.tree.client;
 
+import java.util.List;
+
 import com.raddle.config.tree.api.TreeConfigManager;
 import com.raddle.config.tree.api.TreeConfigNode;
+import com.raddle.config.tree.api.TreeConfigPath;
 
 /**
  * @author xurong
@@ -10,10 +13,24 @@ import com.raddle.config.tree.api.TreeConfigNode;
 public interface TreeConfigClient extends TreeConfigManager {
 	/**
 	 * 绑定断开连接时的值，连接断开后，服务端自动设置这些值,只更新值，不会删除属性
-	 * @param disconnectedValue 端口连接后的值
+	 * @param nodes 端口连接后的节点
 	 * @param includeNodeValue 是否更新节点的值
 	 */
-	public void bindDisconnectedValue(TreeConfigNode disconnectedValue ,boolean includeNodeValue);
+	public void bindDisconnectedNodes(List<TreeConfigNode> nodes ,boolean includeNodeValue);
+	
+	/**
+	 * 绑定连接时，初始化的节点。第一次连接和每次异常断开再连接时，都会重新发送这些值到服务器端，只更新值，不会删除属性
+	 * @param paths 初始化节点路径
+	 * @param recursive 是否递归更新
+	 */
+	public void bindInitialPushNodes(List<TreeConfigPath> paths , boolean recursive);
+	
+	/**
+	 * 绑定连接时，初始化获取的节点。第一次连接和每次异常断开再连接时，都会重新从服务器端获取这些值
+	 * @param paths 初始化节点路径
+	 * @param recursive 是否递归获取
+	 */
+	public void bindInitialGetNodes(List<TreeConfigPath> paths , boolean recursive);
 	
 	/**
 	 * 断开和服务器的连接
