6ace0370f4f989f3036e2473421598b78ff8b122
raddle60 <raddle60@b04a5dc0-1bbb-ae7b-cb6e-5c70aac61664>
2010-12-01 06:30:11 +0000

    配置的变化通知

---
+++ tree-config-api/src/main/java/com/raddle/config/tree/api/TreeConfigListener.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.raddle.config.tree.api;
+
+import java.io.Serializable;
+
+/**
+ * @author xurong
+ * 
+ */
+public interface TreeConfigListener {
+	/**
+	 * 节点新增或修改
+	 * @param node
+	 * @param newValue
+	 * @param oldValue
+	 */
+	public void nodeValueChanged(TreeConfigNode node, Serializable newValue, Serializable oldValue);
+
+	/**
+	 * 节点删除
+	 * @param removedNode
+	 */
+	public void nodeRemoved(TreeConfigNode removedNode);
+
+	/**
+	 * 属性新增或修改
+	 * @param node
+	 * @param attribute
+	 * @param newValue
+	 * @param oldValue
+	 */
+	public void AttributeValueChanged(TreeConfigNode node, TreeConfigAttribute attribute, Serializable newValue, Serializable oldValue);
+
+	/**
+	 * 属性删除
+	 * @param node
+	 * @param removedAttribute
+	 */
+	public void attributeRemoved(TreeConfigNode node, TreeConfigAttribute removedAttribute);
+}
