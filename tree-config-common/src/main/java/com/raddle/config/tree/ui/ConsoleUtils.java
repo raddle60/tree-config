d7d9c7fcba26e6ed91c3f25f2cea4aa4ef5df291
raddle60 <raddle60@b04a5dc0-1bbb-ae7b-cb6e-5c70aac61664>
2010-12-06 03:37:48 +0000

    控制台

--- tree-config-common/src/main/java/com/raddle/config/tree/ui/ConsoleUtils.java
+++ tree-config-common/src/main/java/com/raddle/config/tree/ui/ConsoleUtils.java
@@ -4,6 +4,7 @@
 package com.raddle.config.tree.ui;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PipedInputStream;
@@ -16,6 +17,8 @@ import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import javax.swing.text.JTextComponent;
 
+import org.omg.CORBA.IntHolder;
+
 /**
  * @author xurong
  * 
@@ -24,14 +27,49 @@ public class ConsoleUtils {
 	public static void redirectConsole(final JTextComponent textComponent) {
 		final PipedInputStream pipledInputStream = new PipedInputStream();
 		final PipedOutputStream pipedOutputStream = new PipedOutputStream();
+		// 需要用buffer缓冲
+		// 不用缓冲在swing线程阻塞的情况下，PipedOutputStream会断开pipe
+		final ByteArrayOutputStreamEx byteArrayOutputStream = new ByteArrayOutputStreamEx();
+		final IntHolder bufferSize = new IntHolder(0);
 		try {
 			pipedOutputStream.connect(pipledInputStream);
 		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "连接失败：" + e);
 		}
-		PrintStream ps = new PrintStream(pipedOutputStream);
+		PrintStream ps = new PrintStream(byteArrayOutputStream);
 		System.setOut(ps);
 		System.setErr(ps);
-		Thread outputThread = new Thread() {
+		Thread byteThread = new Thread() {
+			@Override
+			public void run() {
+				while (true) {
+					if (byteArrayOutputStream.size() > 0) { // 检查流里面的字节
+						byte buffer[] = null;
+						synchronized (byteArrayOutputStream) {
+							buffer = byteArrayOutputStream.toByteArray();
+							byteArrayOutputStream.reset(); // 清空缓冲区
+						}
+						try {
+							pipedOutputStream.write(buffer, 0, buffer.length); // 把提取的流发送到pipedOS
+						} catch (IOException e) {
+						}
+					} else {
+						// 没有数据可用，线程进入休眠状态
+						if (byteArrayOutputStream.getBufferSize() > bufferSize.value) {
+							bufferSize.value = byteArrayOutputStream.getBufferSize();
+							System.out.println("current byteArrayOutputStream for console buffer size " + byteArrayOutputStream.getBufferSize());
+						}
+						try {
+							Thread.sleep(500); // 休眠1秒
+						} catch (InterruptedException e) {
+						}
+					}
+				}
+			}
+		};
+		byteThread.setDaemon(true);
+		byteThread.start();
+		Thread pipeThread = new Thread() {
 			@Override
 			public void run() {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(pipledInputStream));
@@ -64,7 +102,18 @@ public class ConsoleUtils {
 				}
 			}
 		};
-		outputThread.setDaemon(true);
-		outputThread.start();
+		pipeThread.setDaemon(true);
+		pipeThread.start();
+	}
+
+	private static class ByteArrayOutputStreamEx extends ByteArrayOutputStream {
+		/**
+		 * 返回buffer的大小，不是有效数据的大小
+		 * 
+		 * @return
+		 */
+		public int getBufferSize() {
+			return buf.length;
+		}
 	}
 }
