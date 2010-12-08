24e2a944cf09de00509c47c837ce35c599ba7663
raddle60 <raddle60@b04a5dc0-1bbb-ae7b-cb6e-5c70aac61664>
2010-12-08 12:58:40 +0000

    使用nio-command的同步调用

--- tree-config-remote/src/main/java/com/raddle/config/tree/remote/SyncCommandSender.java
+++ tree-config-remote/src/main/java/com/raddle/config/tree/remote/SyncCommandSender.java
@@ -54,16 +54,4 @@ public class SyncCommandSender {
 			throw new RemoteExecuteException(e.getType() ,e.getType()+", "+e.getMessage() + ", targetId:" + command.getTargetId() + ", method:" + command.getMethod());
 		}
 	}
-
-	class ObjectHolder {
-		private Object value;
-
-		public Object getValue() {
-			return value;
-		}
-
-		public void setValue(Object value) {
-			this.value = value;
-		}
-	}
 }
